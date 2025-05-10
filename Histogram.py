0c364f00dbe6d3331565396714df7e227b49fe7d
Sumit Sharma <skumar2.0.1800000@gmail.com>
2025-05-10 14:12:52 +0530

    Create  Histogram.py

---
+++ Histogram.py
@@ -0,0 +1,7 @@
+n = [24, 41, 17, 42, 17, 47, 56, 43, 40, 24, 21, 59, 53,25,1,6,45,10,39]
+
+plt.title("Hist Plot",fontsize=20)
+plt.xlabel("Python",fontsize=10)
+plt.ylabel("Score",fontsize=10)
+plt.hist(n,color="c",edgecolor="k")
+plt.show()
